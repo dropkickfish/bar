913433fb8fe814adee57ed8201e7b0f11e005403
33702528 <thackrayaf@gmail.com>
2020-09-23 20:09:11 +0100

    "fixed" status mk2

--- barq-client/staff/src/ui/orderListItem.js
+++ barq-client/staff/src/ui/orderListItem.js
@@ -10,7 +10,7 @@ class OrderListItem extends Component  {
     expanded: false
   }
 
-  statusList = ['paid', 'on its way', 'delivered']
+  statusList = ['ordered', 'on its way', 'delivered']
   onButtonClick = (e) => {
     const index = this.statusList.findIndex(status => status === this.props.status)
     let nextIndex;
